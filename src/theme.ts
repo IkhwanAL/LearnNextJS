fa736f447680a42fc2cfea66bd44ee51928e1afc
ikhwan A Latif <ikhwanal235@gmail.com>
2022-05-13 16:55:35 +0700

    define coior for the website

--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,32 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
+	palette: {
+		primary: {
+			main: "#E4F9F5",
+			light: "#FFFFFF",
+			dark: "#b2c6c2",
+		},
+		secondary: {
+			main: "#30E3CA",
+			light: "#76fffd",
+			dark: "#00b099",
+		},
+		text: {
+			primary: "#000000",
+			secondary: "#000000",
+		},
+		success: {
+			main: "#11999E",
+			light: "#59cacf",
+			dark: "#006a70",
+		},
+		error: {
+			main: "#40514E",
+		},
+	},
 });
 
 export default theme;
